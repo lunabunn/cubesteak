3b6ed95066a9fdcc82d91e8a2adc78cdbf275c76
lunabunn <iamrabbitmoon@gmail.com>
2023-05-20 16:31:42 -0400

    Add help flag and abort on unused args

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ use cubesteak::cubesteak;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = pico_args::Arguments::from_env();
+
+    if args.contains(["-h", "--help"]) {
+        help(0);
+    }
+
     let Ok(source) = args
         .free_from_os_str(|s| PathBuf::try_from(s))
-    else { help() };
+    else { help(1) };
     let Ok(target) = args
         .free_from_os_str(|s| PathBuf::try_from(s))
-    else { help() };
+    else { help(1) };
+    if !args.finish().is_empty() {
+        help(1);
+    }
 
     cubesteak(source, target)
 }
 
-fn help() -> ! {
+fn help(code: i32) -> ! {
     eprintln!("Usage: cubesteak <source> <target>");
-    std::process::exit(1)
+    std::process::exit(code)
 }
